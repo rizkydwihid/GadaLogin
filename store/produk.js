7d40657d0630204943cad04cc1875b40509897d1
Rizky Dwi Hidayat <rizkydwihidayat@Rizkys-Air.local>
2019-04-25 18:07:57 +0700

    store, dynamic route, axios

---
+++ store/produk.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+    all: [
+        {
+            name: "gadget",
+            content: "smartphone, laptop"
+        },
+        {
+            name: "aksesoris",
+            content: "earphone, powerbank, cover case"
+        }
+    ]
+})
